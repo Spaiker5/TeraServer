ed57e6783ced9525224b898b3791c11bae0a233a
Spaiker <Stahu555@gmail.com>
2022-02-05 07:29:08 +0100

    Add, basic connection and first draft on GUI.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import io
 import json
 
-from flask import Flask, request
+from flask import request, Flask, render_template, Response
 from flask_sqlalchemy import SQLAlchemy
+from matplotlib.figure import Figure
+from matplotlib_inline.backend_inline import FigureCanvas
 
 app = Flask("Api")
 app.config['SECRET_KEY'] = 'secret'
@@ -13,10 +16,10 @@ db = SQLAlchemy(app)
 
 
 # CONFIGURE TABLE
-class TereriumData(db.Model):
+class TeraData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     snake = db.Column(db.String(50), nullable=False)
-    temp = db.Column(db.Float, nullable=False)
+    temp = db.Column(db.Integer, nullable=False)
     date = db.Column(db.String(100), nullable=False)
 
 
@@ -28,7 +31,7 @@ def sensors():
     json_data = request.get_json(force=True)
     data = json.loads(json_data)
 
-    new_entry = TereriumData(
+    new_entry = TeraData(
         snake=data["Snake"],
         temp=data["Temp"],
         date=data["Data"],
@@ -38,9 +41,41 @@ def sensors():
     return data
 
 
-@app.route("/staty")
-def staty():
-    pass
+#
+# @app.route("/")
+# def staty():
+#     for snake in TeraData.query.group_by(TeraData.snake).all():
+#         query = TeraData.query.order_by(TeraData.id.desc()).first()
+#         print(query)
+#     return render_template("template.html", temp=query.temp, date=query.date)
+
+
+@app.route("/name/<snake>")
+def staty_snake(snake):
+    query = TeraData.query.filter(TeraData.snake == snake).order_by(TeraData.id.desc()).first()
+    return render_template("template.html", temp=query.temp, date=query.date, snake=snake)
+
+
+@app.route('/plot.png')
+def plot_png():
+    fig = Figure(figsize=[7, 3], facecolor="#f5700c", edgecolor="#f5700c")
+    ax = fig.subplots()
+    ax.patch.set_facecolor("#f5700c")
+
+    x = []
+    y = []
+    for date in TeraData.query.all():
+        date = date.date
+        x.append(date)
+    for temp in TeraData.query.all():
+        temp = temp.temp
+        y.append(temp)
+
+    # print(x, y, TeraData.query.all())
+    ax.plot(x, y)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 
 app.run(debug=True)
