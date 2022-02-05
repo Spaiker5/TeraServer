d77541426abc8b0e90e29c20c5a251046fb256dd
Spaiker <Stahu555@gmail.com>
2022-02-05 22:07:31 +0100

    Fix ploting for each snake.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tererium.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+# TODO: User login and verification. Snake list relationship to it.
 
 # CONFIGURE TABLE
 class TeraData(db.Model):
@@ -41,13 +41,11 @@ def sensors():
     return data
 
 
-#
-# @app.route("/")
-# def staty():
-#     for snake in TeraData.query.group_by(TeraData.snake).all():
-#         query = TeraData.query.order_by(TeraData.id.desc()).first()
-#         print(query)
-#     return render_template("template.html", temp=query.temp, date=query.date)
+@app.route("/")
+def staty():
+    print()
+    # TODO: All snake's overview.
+    # return render_template("template.html", temp=query.temp, date=query.date)
 
 
 @app.route("/name/<snake>")
@@ -56,22 +54,24 @@ def staty_snake(snake):
     return render_template("template.html", temp=query.temp, date=query.date, snake=snake)
 
 
-@app.route('/plot.png')
-def plot_png():
+@app.route("/<snake>/plot.png")
+def plot_png(snake):
     fig = Figure(figsize=[7, 3], facecolor="#f5700c", edgecolor="#f5700c")
     ax = fig.subplots()
     ax.patch.set_facecolor("#f5700c")
 
     x = []
     y = []
-    for date in TeraData.query.all():
+    for date in TeraData.query.filter(TeraData.snake == snake).all():
         date = date.date
+        print(type(date))
+
         x.append(date)
-    for temp in TeraData.query.all():
+    for temp in TeraData.query.filter(TeraData.snake == snake).all():
         temp = temp.temp
         y.append(temp)
 
-    # print(x, y, TeraData.query.all())
+    print(x, y)
     ax.plot(x, y)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
