c02ba3834caf5245f5d52b78b83e0818a9843907
Spaiker <Stahu555@gmail.com>
2022-02-08 22:46:01 +0100

    Snakes by login user, list of recent data received.

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from functools import wraps
 from flask import request, Flask, render_template, redirect, Response, flash, url_for, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_login import UserMixin, login_user, LoginManager, logout_user
+from flask_login import login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
 from matplotlib.figure import Figure
 from matplotlib_inline.backend_inline import FigureCanvas
-from sqlalchemy.orm import relationship
-from werkzeug.security import check_password_hash
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from forms import LoginForm, RegisterForm
 
@@ -33,30 +32,6 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-# USER TABLE
-class User(UserMixin, db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
-    snakes = relationship("Snake", back_populates="owner")
-
-
-# CONFIGURE TABLE
-class Snake(db.Model):
-    __tablename__ = "snake"
-    id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    owner = relationship("User", back_populates="snakes")
-    name = db.Column(db.String(50), nullable=False)
-    temp = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.String(100), nullable=False)
-
-
-db.create_all()
-
-
 def admin_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -65,6 +40,28 @@ def admin_only(f):
         return f(*args, **kwargs)
 
     return decorated_function
+
+
+# USER TABLE
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(50), nullable=False)
+
+
+# CONFIGURE TABLE
+class Snake(db.Model):
+    __tablename__ = "snake"
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    name = db.Column(db.String(50), nullable=False)
+    temp = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.String(100), nullable=False)
+
+
+db.create_all()
 
 
 @app.route("/sensors", methods=["POST"])
@@ -76,6 +73,7 @@ def sensors():
         name=data["Snake"],
         temp=data["Temp"],
         date=data["Data"],
+        owner_id=data["Owner_ID"]
     )
     db.session.add(new_entry)
     db.session.commit()
@@ -83,16 +81,51 @@ def sensors():
 
 
 @app.route("/")
-def staty():
-    print()
+def welcome_page():
     # TODO: All name's overview.
-    # return render_template("template.html", temp=query.temp, date=query.date)
+    return render_template("dashboard.html")
 
 
-@app.route("/name/<name>")
-def staty_snake(name):
+@app.route("/list")
+@login_required
+def lista():
+    user_id = current_user.id
+    snakes = Snake.query.filter(Snake.owner_id == user_id).order_by(Snake.date.desc())
+
+    return render_template("list.html", snakes=snakes)
+
+
+@app.route("/snakes")
+@login_required
+def all_snakes():
+    user_id = current_user.id
+    snakes = Snake.query.filter(Snake.owner_id == user_id)
+
+
+    return render_template("snakes.html", snakes=snakes)
+
+
+@app.route("/snake/<name>")
+@login_required
+def snake_select_name(name):
+    user_id = current_user.id
     query = Snake.query.filter(Snake.name == name).order_by(Snake.id.desc()).first()
-    return render_template("template.html", temp=query.temp, date=query.date, snake=name)
+    if query.owner_id == user_id:
+        return render_template("single_overview.html", temp=query.temp, date=query.date, snake=name)
+    else:
+        flash("Nie znaleziono węża o takim imieniu.")
+        return redirect(url_for('welcome_page'))
+
+
+@app.route("/snake/<snake_id>")
+@login_required
+def snake_select_id(snake_id):
+    user_id = current_user.id
+    query = Snake.query.filter(Snake.id == snake_id).order_by(Snake.id.desc()).first()
+    if query.owner_id == user_id:
+        return render_template("single_overview.html", temp=query.temp, date=query.date, snake=query.name)
+    else:
+        return flash("Nie znaleziono węża o takim ID.")
 
 
 @app.route("/<name>/plot.png")
